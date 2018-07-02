e2d788940c340b4f22f1ff565b48459dfd044a54
Abhishek Pandit <38048933+abhishekiiitbh@users.noreply.github.com>
2018-07-02 16:04:53 +0530

    Add files via upload

---
+++ Crop_Object.py
@@ -0,0 +1,85 @@
+
+# Capture the mouse click events in Python and OpenCV
+'''
+-> draw shape on any image 
+-> reset shape on selection
+-> crop the selection
+
+Run this code on Python Terminal , 
+1.Just change the directory to open this Crop_Object.py file 
+2.The frames should be in the same directory as Crop_Object.py file 
+Take the frames that you wanna crop one by one and save it in the same directory!
+Don't forget to change the name of cropped image , as when you will save the next cropped frame ,
+it will overwrite, so to avoid that, just rename!
+run the code : python capture_events.py --image jimgf148.jpg
+@author: Alan Darko , IIIT Bh
+'''
+
+
+# import the necessary packages
+import argparse
+import cv2
+
+# initialize the list of reference points and boolean indicating
+# whether cropping is being performed or not
+ref_point = []
+cropping = False
+
+def shape_selection(event, x, y, flags, param):
+	# grab references to the global variables
+	global ref_point, cropping
+
+	# if the left mouse button was clicked, record the starting
+	# (x, y) coordinates and indicate that cropping is being
+	# performed
+	if event == cv2.EVENT_LBUTTONDOWN:
+		ref_point = [(x, y)]
+		cropping = True
+
+	# check to see if the left mouse button was released
+	elif event == cv2.EVENT_LBUTTONUP:
+		# record the ending (x, y) coordinates and indicate that
+		# the cropping operation is finished
+		ref_point.append((x, y))
+		cropping = False
+
+		# draw a rectangle around the region of interest
+		cv2.rectangle(image, ref_point[0], ref_point[1], (0, 255, 0), 2)
+		cv2.imshow("image", image)
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+
+# load the image, clone it, and setup the mouse callback function
+image = cv2.imread(args["image"])
+clone = image.copy()
+cv2.namedWindow("image")
+cv2.setMouseCallback("image", shape_selection)
+
+# keep looping until the 'q' key is pressed
+while True:
+	# display the image and wait for a keypress
+	cv2.imshow("image", image)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the 'r' key is pressed, reset the cropping region
+	if key == ord("r"):
+		image = clone.copy()
+
+	# if the 'c' key is pressed, break from the loop
+	elif key == ord("c"):
+		break
+
+# if there are two reference points, then crop the region of interest
+# from teh image and display it
+if len(ref_point) == 2:
+	crop_img = clone[ref_point[0][1]:ref_point[1][1], ref_point[0][0]:ref_point[1][0]]
+	cv2.imshow("crop_img", crop_img)
+    
+	cv2.waitKey(0)
+
+# close all open windows
+cv2.destroyAllWindows()
+cv2.imwrite("crop.jpg", crop_img) 
